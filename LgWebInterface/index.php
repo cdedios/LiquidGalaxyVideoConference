63b80f1b26ee71a9868727cf10e44e4ff97f9a59
Carlos de Dios <cdedios92@gmail.com>
2014-07-21 17:16:56 +0200

    LG web interface to get inside te video call

---
+++ LgWebInterface/index.php
@@ -0,0 +1,26 @@
+<?php?>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <script type="text/javascript" src="main.js"></script>
+
+    <title>Liquid Galaxy Hangouts</title>
+  </head>
+    <nav>
+		  <ul>
+				<h1>Liquid Galaxy Hangouts</h1>
+				 	 <li><a href="#" onclick="sendQuery('relaunch');">Relaunch</a></li>
+					 <li><a href="#" onclick="sendQuery('reboot');">Reboot</a></li>
+					 <li><a href="#" onclick="sendQuery('shutdown');">Shutdown</a></li>
+					 <li><a href="#" onclick="sendQuery('perusearue');">Peruse-a-rue</a></li>
+					 <li><a href="http://vm218.endpoint.com:3001/touchscreen" onclick="sendQuery('relaunch');">VideoCall</a></li>
+
+		  </ul>
+    </nav>
+
+  <body>
+    <div class="touchscreen">
+      <div id="status"></div>
+    </div>
+  </body>
+</html>
