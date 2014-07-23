32df22a20a96455900a6b57cd0c5209a19cbd8af
Carlos de Dios <cdedios92@gmail.com>
2014-07-23 13:21:51 +0200

    updated script to open the chrome browser with the fixed url over all the LG

--- scripts/lg-videocall.sh
+++ scripts/lg-videocall.sh
@@ -7,18 +7,10 @@
 
 lg-ctl-master
 
-#GEScriptPID=$(pgrep run-earth-bin)
-ssh -tt -x lg@lg1 "killall -q node; liquid-galaxy.lg-peruse-a-rue/bin/peruse-a-rue" &
-lg-run "killall -q run-earth-bin.sh; killall -q chromium-browser; killall -q googleearth-bin; killall -q google-chrome"
+ssh -tt -x lg@lg1 lg-run "killall -q run-earth-bin.sh; killall -q chromium-browser; killall -q googleearth-bin; killall -q google-chrome"
 
 for lg in $LG_FRAMES ; do
-	frame=$(($(echo $lg | cut -c 3)-1))
-
-	if [[ ${frame} -gt $(( ${LG_FRAMES_MAX}/2 )) ]] ; then
-	    frame="$(( ${frame} - ${LG_FRAMES_MAX} ))"
-	fi
-
-	CMD0="export DISPLAY=:0 && google-chrome vm218.endpoint.com:3001/lg"
+	CMD="export DISPLAY=:0 && google-chrome vm218.endpoint.com:3001/lg"
 
 ssh -x lg@$lg "$CMD" &
 
